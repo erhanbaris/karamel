d09ae77368cda6900703739d88d6ed30a538df52
Erhan BARIS <erhanbaris@gmail.com>
2020-11-08 22:11:37 +0100

    Some changes

--- src/syntax/mod.rs
+++ src/syntax/mod.rs
@@ -2,23 +2,26 @@ use std::vec::{Vec};
 use std::str::Chars;
 use std::iter::Peekable;
 use std::slice::Iter;
+use std::cell::Cell;
+
 
 use crate::types::*;
 
 pub struct SyntaxParser<'a>  {
     tokens: &'a Vec<Token>,
-    index: usize
+    index: Cell<usize>,
+
 }
 
 impl<'a> SyntaxParser<'a> {
     pub fn new(tokens: &'a Vec<Token>) -> SyntaxParser {
         SyntaxParser {
             tokens: tokens,
-            index: 0
+            index: Cell::new(0)
         }
     }
 
-    pub fn parse(&mut self) {
+    pub fn parse(&self) {
     }
 
     pub fn is_primative(&self) -> bool {
@@ -60,27 +63,27 @@ impl<'a> SyntaxParser<'a> {
     }
 
     fn peek_ast(&self) -> Option<&Token> {
-        self.tokens.get(self.index)
+        self.tokens.get(self.index.get())
     }
 
     fn previous_ast(&self) -> Option<&Token> {
-        self.tokens.get(self.index -1)
+        self.tokens.get(self.index.get() - 1)
     }
 
     fn next_ast(&self) -> Option<&Token> {
-        self.tokens.get(self.index + 1)
+        self.tokens.get(self.index.get() + 1)
     }
 
-    fn increase(&mut self) {
-        self.index += 1;
+    fn increase(&self) {
+        self.index.set(self.index.get() + 1);
     }
 
-    fn consume_ast(&mut self) -> Option<&Token> {
-        self.index += 1;
-        self.tokens.get(self.index)
+    fn consume_ast(&self) -> Option<&Token> {
+        self.index.set(self.index.get() + 1);
+        self.tokens.get(self.index.get())
     }
 
-    pub fn primary_expr(&mut self) -> AstResult {
+    pub fn primary_expr(&self) -> AstResult {
         if self.is_primative() {
             if let Some(token) = self.peek_ast() {
                 return self.create_primative_ast(token);
@@ -90,7 +93,7 @@ impl<'a> SyntaxParser<'a> {
         Err((String::from("not implemented"), 0, 0))
     }
 
-    fn create_primative_ast(&mut self, token :&Token) -> AstResult {
+    fn create_primative_ast(&self, token :&Token) -> AstResult {
         let ast = match &token.token_type {
             BramaTokenType::Integer(int)      => BramaAstType::Primative(BramaPrimative::Integer(*int)),
             BramaTokenType::Double(double)    => BramaAstType::Primative(BramaPrimative::Double(*double)),
@@ -107,7 +110,7 @@ impl<'a> SyntaxParser<'a> {
                     if let Some(next_token) = self.next_ast() {
                         match &next_token.token_type {
                             BramaTokenType::Symbol(symbol) => {
-                                self.increase();
+                                self.consume_ast();
                                 return Ok(BramaAstType::Primative(BramaPrimative::Atom(symbol.to_string())));
                             },
                             _ => {
