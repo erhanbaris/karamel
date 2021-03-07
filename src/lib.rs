b358e25abb488f03b0179a5f7fde9e4404013e91
Erhan BARIS <erhanbaris@gmail.com>
2021-03-07 20:34:03 +0100

    New library created to seperate web and executable

--- src/lib.rs
+++
@@ -1,8 +0,0 @@
-#[macro_use]
-pub mod macros;
-pub mod parser;
-pub mod syntax;
-pub mod types;
-pub mod vm;
-pub mod compiler;
-pub mod buildin;
--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,7 @@
-extern crate bitflags;
-extern crate log_update;
+extern crate libtpd;
 
-mod macros;
-mod types;
-mod parser;
-mod syntax;
-mod vm;
-mod compiler;
-mod buildin;
-
-#[cfg(feature = "wasmBuild")]
-use wasm_bindgen::prelude::*;
-
-#[cfg(feature = "wasmBuild")]
-#[wasm_bindgen]
-extern {
-    pub fn alert(s: &str);
-}
-
-#[cfg(feature = "wasmBuild")]
-#[wasm_bindgen]
-pub fn greet(name: &str) {
-    alert(&format!("Hello, {}!", name));
-}
-
-#[cfg(not(feature = "wasmBuild"))]
 fn main() {
-    let result = vm::executer::code_executer(&r#"
-fn test:
-    fn test_erhan:
-        döndür 'erhan'
-    döndür test_erhan
-
-data      = { }
-data.func = test
-
-hataayıklama::doğrula(data.func()(), 'erhan')"#.to_string());
+    let result = libtpd::vm::executer::code_executer(&r#"1025*2"#.to_string());
     match result {
         Ok(_) => println!("Success"),
         Err(error) => println!("Fail ({})", error)
