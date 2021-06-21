903a50977019bd232352004bdaa267bce5c560c1
Ruslan Asenov <Ruslan.Asenov@motorolasolutions.com>
2021-06-21 16:50:13 +0200

    .

--- karamellib/src/compiler/module.rs
+++ karamellib/src/compiler/module.rs
@@ -18,16 +18,18 @@ pub struct OpcodeModule {
     pub name: String,
     pub module_path: String,
     pub main_ast: BramaAstType,
-    pub functions: HashMap<String, Rc<FunctionReference>>
+    pub functions: HashMap<String, Rc<FunctionReference>>,
+    pub modules: HashMap<String, Rc<dyn Module>>
 }
 
 impl OpcodeModule {
-    pub fn new(name: String, module_path: String, main_ast: BramaAstType, functions: HashMap<String, Rc<FunctionReference>>) -> OpcodeModule {
+    pub fn new(name: String, module_path: String, main_ast: BramaAstType, functions: HashMap<String, Rc<FunctionReference>>, modules: HashMap<String, Rc<dyn Module>>) -> OpcodeModule {
         OpcodeModule {
             name, 
             module_path, 
             main_ast,
-            functions
+            functions,
+            modules
         }
     }
 }
@@ -88,8 +90,10 @@ pub fn load_module(params: &[String], options: &mut BramaCompiler) -> Result<Opc
         return match syntax.parse() {
             Ok(ast) => {
                 let mut functions : HashMap<String, Rc<FunctionReference>> = HashMap::new();
+                let mut modules : HashMap<String, Rc<dyn Module>> = HashMap::new();
                 find_function_definition_type(&ast, &mut functions, options, 0)?;
-                let module = OpcodeModule::new(module, path.to_str().unwrap().to_string(), ast, functions);
+                find_module_load_type(&ast, &mut modules, options, 0)?;
+                let module = OpcodeModule::new(module, path.to_str().unwrap().to_string(), ast, functions, modules);
                 Ok(module)
             },
             Err(error) => return Err(generate_error_message(&content, &error))
@@ -125,6 +129,27 @@ fn find_load_type(ast: &BramaAstType, options: &mut BramaCompiler, modules: &mut
     Ok(())
 }
 
+fn find_module_load_type(ast: &BramaAstType, modules: &mut HashMap<String, Rc<dyn Module>>, options: &mut BramaCompiler, depth_level: usize) -> CompilerResult {
+    if depth_level > 1 {
+        return Ok(());
+    }
+/*
+    match ast {
+        BramaAstType::Load(module_name) => {
+            let function_reference = FunctionReference::opcode_function(name.to_string(), arguments.to_vec(), Vec::new(),  0, 0);
+            modules.insert(name.to_string(), function_reference);
+        },
+        BramaAstType::Block(blocks) => {
+            for block in blocks {
+                find_module_load_type(block, modules, options, depth_level + 1)?;
+            }
+        },
+        _ => ()
+    }*/
+
+    Ok(())
+}
+
 fn find_function_definition_type(ast: &BramaAstType, functions: &mut HashMap<String, Rc<FunctionReference>>, options: &mut BramaCompiler, depth_level: usize) -> CompilerResult {
     if depth_level > 1 {
         return Ok(());
@@ -151,5 +176,76 @@ pub fn get_modules(main_ast: &BramaAstType, options: &mut BramaCompiler) -> Resu
     match find_load_type(main_ast, options, &mut modules, 0) {
         Ok(()) => Ok(modules),
         Err(error) => Err(error)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::panic;
+    use std::fs::File;
+    use std::io::prelude::*;
+    use std::path::Path;
+
+    fn setup() {
+
+        let current_exe= std::env::current_exe().unwrap();
+
+        println!("{:?}", current_exe);
+        println!("{:?}", current_exe.parent());
+        println!("setup");
+    }
+
+    fn teardown(to_be_removed: Vec<String>) {
+        let parent_path = std::env::current_exe().unwrap().parent().unwrap();
+        for file in to_be_removed.iter() {
+            let path = parent_path.clone().join(file);
+            std::fs::remove_file(path);
+        }
+        println!("teardown");
+    }
+
+    fn run_test<T>(test: T, to_be_removed: Vec<String>) -> ()
+        where T: FnOnce() -> () + panic::UnwindSafe
+    {
+        setup();
+
+        let result = panic::catch_unwind(|| {
+            test()
+        });
+
+        teardown(to_be_removed);
+
+        assert!(result.is_ok())
+    }
+
+    fn write_to_file(content: &'static str, file_name: &'static str) -> String {
+        let file_name = generate_file_name(file_name);
+        let mut file = File::create(&file_name).unwrap();
+        file.write_all(content.as_bytes()).unwrap();
+        file_name
+    }
+
+    fn generate_file_name(file_name: &'static str) -> String {
+        let parent = match std::env::current_exe() {
+            Ok(path) => match path.parent() {
+                Some(parent_path) => parent_path,
+                _ => Path::new(".")
+            },
+            _ => Path::new(".")
+        };
+        
+        parent.clone().join(file_name).to_str().unwrap().to_string()
+    }
+
+    #[test]
+    fn test_1() {
+
+        let module_1 = r#"
+fonk topla(bir, iki): dondur bir + iki"#;
+        write_to_file(module_1, "topla");
+
+        run_test(|| {    
+            assert!(false);
+        }, [String::from("foo.tpd")].to_vec());
     }
 }
