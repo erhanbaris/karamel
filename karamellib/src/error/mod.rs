d5f0389954561f36168490f3a198517152129a36
Ruslan Asenov <Ruslan.Asenov@motorolasolutions.com>
2021-07-05 15:47:42 +0200

    Hata sinifi uzerinde duzenlemeler yapildi ve yeni test kodlari eklendi.

--- karamellib/src/error/mod.rs
+++ karamellib/src/error/mod.rs
@@ -1,43 +1,153 @@
+use std::borrow::Borrow;
+
+use strum::EnumMessage;
+use strum_macros::EnumIter;
+use strum_macros::EnumMessage;
+use thiserror::Error;
 
 #[derive(Copy)]
 #[derive(Clone)]
 #[derive(Debug)]
 #[derive(PartialEq)]
+#[derive(Error)]
+#[derive(EnumIter)]
+#[derive(EnumMessage)]
 pub enum KaramelErrorType {
-    SyntaxError = 100,
-    InvalidExpression = 101,
-    MoreThan1ArgumentPassed = 102,
-    RightParanthesesMissing = 103,
-    AssertFailed = 104,
-    NumberNotParsed = 105,
-    MissingStringDeliminator = 106,
-    CharNotValid = 107,
-    RightSideOfExpressionNotFound = 108,
-    ReturnMustBeUsedInFunction = 109,
-    FunctionCallSyntaxNotValid = 110,
-    FunctionNameNotDefined = 111,
-    ArgumentMustBeText = 112,
-    IfConditionBodyNotFound = 113,
-    ParenthesesNotClosed = 114,
-    InvalidUnaryOperation = 115,
-    UnaryWorksWithNumber = 116,
-    ArgumentNotFound = 117,
-    MultipleElseUsageNotValid = 118,
-    BreakAndContinueBelongToLoops = 119,
-    FunctionConditionBodyNotFound = 120,
-    ColonMarkMissing = 121,
-    ElseIsUsed = 122,
-    IndentationIssue = 123,
-    DictNotClosed = 124,
-    ArrayNotClosed = 125,
-    InvalidListItem = 126,
-    DictionaryKeyNotValid = 127,
-    DictionaryValueNotValid = 128,
-    CommentNotFinished = 129,
-    WhileStatementNotValid = 130,
-    FunctionDefinationNotValid = 131,
-    MissingIf = 132,
-    KeywordCouldNotBeUsed = 133
+    #[error("Sozdizimi hatasi")]
+    #[strum(message = "100")]
+    SyntaxError,
+    
+    #[error("Geçersiz ifade")]
+    #[strum(message = "101")]
+    InvalidExpression,
+
+    #[error("Birden fazla degisken kullanilamaz")]
+    #[strum(message = "102")]
+    MoreThan1ArgumentPassed,
+    
+    #[error("Sağ parantaz eksik")]
+    #[strum(message = "103")]
+    RightParanthesesMissing,
+    
+    #[error("Doğrulanamadı")]
+    #[strum(message = "104")]
+    AssertFailed,
+    
+    #[error("Sayı ayrıştırılamadı")]
+    #[strum(message = "105")]
+    NumberNotParsed,
+    
+    #[error("Yazı sonlandırıcısı bulunamadı")]
+    #[strum(message = "106")]
+    MissingStringDeliminator,
+    
+    #[error("Karakter geçerli değil")]
+    #[strum(message = "107")]
+    CharNotValid,
+    
+    #[error("İfadenin sağ tarafı bulunamadı")]
+    #[strum(message = "108")]
+    RightSideOfExpressionNotFound,
+    
+    #[error("Döndür komutu fonksiyon içinde kullanılmalıdır")]
+    #[strum(message = "109")]
+    ReturnMustBeUsedInFunction,
+    
+    #[error("Fonksiyon çağırma sözdizimi geçerli değil")]
+    #[strum(message = "110")]
+    FunctionCallSyntaxNotValid,
+    
+    #[error("Fonksiyon adı tanımlanmamış")]
+    #[strum(message = "111")]
+    FunctionNameNotDefined,
+    
+    #[error("Değişken yazı olmalıdır")]
+    #[strum(message = "112")]
+    ArgumentMustBeText,
+    
+    #[error("Koşul gövdesi eksik")]
+    #[strum(message = "113")]
+    IfConditionBodyNotFound,
+    
+    #[error("Parantez kapatılmamış")]
+    #[strum(message = "114")]
+    ParenthesesNotClosed,
+    
+    #[error("Geçersiz tekli işlem")]
+    #[strum(message = "115")]
+    InvalidUnaryOperation,
+    
+    #[error("Tekli numara ile çalışmaktadır")]
+    #[strum(message = "116")]
+    UnaryWorksWithNumber,
+    
+    #[error("Parametre bulunamadı")]
+    #[strum(message = "117")]
+    ArgumentNotFound,
+    
+    #[error("Birden fazla 'yoksa' ifadesi kullanılamaz")]
+    #[strum(message = "118")]
+    MultipleElseUsageNotValid,
+    
+    #[error("'kır' ve 'devam' ifadeleri döngü içinde kullanılabilir")]
+    #[strum(message = "119")]
+    BreakAndContinueBelongToLoops,
+    
+    #[error("Fonksiyon içi kodlar bulunamadı")]
+    #[strum(message = "120")]
+    FunctionConditionBodyNotFound,
+    
+    #[error("':' eksik")]
+    #[strum(message = "121")]
+    ColonMarkMissing,
+    
+    #[error("'yoksa' zaten kullanıldı")]
+    #[strum(message = "122")]
+    ElseIsUsed,
+    
+    #[error("Girinti sorunu")]
+    #[strum(message = "123")]
+    IndentationIssue,
+    
+    #[error("Sözlük düzgün kapatılmamış")]
+    #[strum(message = "124")]
+    DictNotClosed,
+    
+    #[error("Dizi düzgün kapatılmadı")]
+    #[strum(message = "125")]
+    ArrayNotClosed,
+    
+    #[error("Dizi elemanı geçersiz")]
+    #[strum(message = "126")]
+    InvalidListItem,
+    
+    #[error("Sözlük anahtarı geçersiz")]
+    #[strum(message = "127")]
+    DictionaryKeyNotValid,
+    
+    #[error("Sözlük geçeri geçersiz")]
+    #[strum(message = "128")]
+    DictionaryValueNotValid,
+    
+    #[error("Yorum bilgisi düzgün kapatılmadı")]
+    #[strum(message = "129")]
+    CommentNotFinished,
+    
+    #[error("Döngü düzgün tanımlanmamış")]
+    #[strum(message = "130")]
+    WhileStatementNotValid,
+    
+    #[error("Fonksiyon tanımlaması hatalı")]
+    #[strum(message = "131")]
+    FunctionDefinationNotValid,
+    
+    #[error("'ise' sözcüğü eksik")]
+    #[strum(message = "132")]
+    MissingIf,
+    
+    #[error("Anahtar kelimeler kullanılamaz")]
+    #[strum(message = "133")]
+    KeywordCouldNotBeUsed
 }
 
 #[derive(Copy)]
@@ -50,50 +160,47 @@ pub struct KaramelError {
     pub column: u32
 }
 
-pub fn generate_error_message(data: &String, error: &KaramelError) -> String {
-    let lines = data.split(|c| c == '\n').collect::<Vec<_>>();
-    let line = lines.iter().skip(error.line as usize).next().unwrap();
-    return format!("...\r\n{}\r\n{:>width$} [{}:{}] {}", line, "^", error.line, error.column, error.error_type.as_text(),  width=error.column as usize);
+pub fn generate_error_message<T: AsRef<str>, E: Borrow<KaramelError>>(data: T, error: E) -> String {
+    let error_ref = error.borrow();
+    let lines = data.as_ref().split(|c| c == '\n').collect::<Vec<_>>();
+    let line = lines.iter().skip(error_ref.line as usize).next().unwrap();
+    return format!("...\r\n{}\r\n{:>width$} [{}:{}] (#{}) {}", line, "^", error_ref.line, error_ref.column, error_ref.error_type.get_message().unwrap(), error_ref.error_type,  width=error_ref.column as usize);
 }
 
-impl KaramelErrorType {
-    pub fn as_text(&self) -> String {
-        let message = match self {
-            KaramelErrorType::SyntaxError => "Sozdizimi hatasi",
-            KaramelErrorType::MoreThan1ArgumentPassed => "Birden fazla degisken kullanilamaz",
-            KaramelErrorType::RightParanthesesMissing => "Sağ parantaz eksik",
-            KaramelErrorType::AssertFailed => "Doğrulanamadı",
-            KaramelErrorType::NumberNotParsed => "Sayı ayrıştırılamadı",
-            KaramelErrorType::MissingStringDeliminator => "Yazı sonlandırıcısı bulunamadı",
-            KaramelErrorType::CharNotValid => "Karakter geçerli değil",
-            KaramelErrorType::RightSideOfExpressionNotFound => "İfadenin sağ tarafı bulunamadı",
-            KaramelErrorType::ReturnMustBeUsedInFunction => "Döndür komutu fonksiyon içinde kullanılmalıdır",
-            KaramelErrorType::FunctionCallSyntaxNotValid => "Fonksiyon çağırma sözdizimi geçerli değil",
-            KaramelErrorType::FunctionNameNotDefined => "Fonksiyon adı tanımlanmamış",
-            KaramelErrorType::ArgumentMustBeText => "Değişken yazı olmalıdır",
-            KaramelErrorType::IfConditionBodyNotFound => "Koşul gövdesi eksik",
-            KaramelErrorType::ParenthesesNotClosed => "Parantez kapatılmamış",
-            KaramelErrorType::InvalidUnaryOperation => "Geçersiz tekli işlem",
-            KaramelErrorType::UnaryWorksWithNumber => "Tekli numara ile çalışmaktadır",
-            KaramelErrorType::InvalidExpression => "Geçersiz ifade",
-            KaramelErrorType::ArgumentNotFound => "Parametre bulunamadı",
-            KaramelErrorType::MultipleElseUsageNotValid => "Birden fazla 'yoksa' ifadesi kullanılamaz",
-            KaramelErrorType::BreakAndContinueBelongToLoops => "'kır' ve 'devam' ifadeleri döngü içinde kullanılabilir",
-            KaramelErrorType::FunctionConditionBodyNotFound => "Fonksiyon içi kodlar bulunamadı",
-            KaramelErrorType::ColonMarkMissing => "':' eksik",
-            KaramelErrorType::ElseIsUsed => "'yoksa' zaten kullanıldı",
-            KaramelErrorType::IndentationIssue => "Girinti sorunu",
-            KaramelErrorType::DictNotClosed => "Sözlük düzgün kapatılmamış",
-            KaramelErrorType::ArrayNotClosed => "Dizi düzgün kapatılmadı",
-            KaramelErrorType::InvalidListItem => "Dizi elemanı geçersiz",
-            KaramelErrorType::DictionaryKeyNotValid => "Sözlük anahtarı geçersiz",
-            KaramelErrorType::DictionaryValueNotValid => "Sözlük geçeri geçersiz",
-            KaramelErrorType::CommentNotFinished => "Yorum bilgisi düzgün kapatılmadı",
-            KaramelErrorType::WhileStatementNotValid => "Döngü düzgün tanımlanmamış",
-            KaramelErrorType::FunctionDefinationNotValid => "Fonksiyon tanımlaması hatalı",
-            KaramelErrorType::MissingIf => "'ise' sözcüğü eksik",
-            KaramelErrorType::KeywordCouldNotBeUsed => "Anahtar kelimeler kullanılamaz"
-        };
-        format!("(#{}) {}", *self as u8, message)
+#[cfg(test)]
+mod test {
+    use strum::IntoEnumIterator;
+    use strum::EnumMessage;
+
+    #[test]
+    fn test_all_error_has_number() {
+        for error_enum in super::KaramelErrorType::iter() {
+            let error_message = format!("{}", error_enum);
+
+            if error_message.len() == 0 {
+                assert!(false, "'{:?}' hata mesaji yok", error_enum)
+            }
+
+            match error_enum.get_message() {
+                Some(error_message) => assert!(error_message.len() > 0, "'{:?}' sira numarasi bos olamaz", error_enum),
+                None => assert!(false, "{} sira numarasi yok", error_enum)
+            }
+        }
+    }
+    #[test]
+    fn test_error_message_generator() {
+        let error_info = super::KaramelError {
+            error_type: super::KaramelErrorType::SyntaxError,
+             line: 0,
+             column: 0
+         };
+        let error_message = super::generate_error_message("merhaba dunya", &error_info);
+
+        assert!(error_message.len() > 0, "Hata mesaji bos uretilemez");
+        assert!(error_message.contains("merhaba dunya"), "Hata mesaji icerisinde kaynak kod bilgisi yok");
+        assert!(error_message.contains(error_info.error_type.get_message().unwrap()), "Mesaj icerisinde hata kodu yok");
+
+        let error_message = format!("{}", error_info.error_type);
+        assert!(error_message.contains(&error_message), "Mesaj icerisinde hata kodu mesaji");
     }
 }
